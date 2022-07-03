07d5f81c7f3d853d732e1e0bb51b321cc251c71e
Piotr Jezusek <piotr.jezusek@softbeam.pl>
2022-07-03 20:23:51 +0200

    Add nvim-tree and change mapping for easymotion

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -95,4 +95,13 @@ return require('packer').startup(function()
     "folke/todo-comments.nvim",
     requires = "nvim-lua/plenary.nvim",
   }
+
+  -- Tree view
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icons
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
 end)

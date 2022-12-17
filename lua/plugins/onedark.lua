5bc460a9eb463553037572aafd1add780d15155e
Piotr Jezusek <piotr.jezusek@softbeam.pl>
2022-12-17 15:09:56 +0100

    Some theme updates

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -3,30 +3,26 @@ require('onedark').setup {
   style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
   transparent = false, -- Show/hide background
   term_colors = true, -- Change terminal color as per the selected theme style
-  ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+  ending_tildes = true, -- Show the end-of-buffer tildes. By default they are hidden
   cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
   -- toggle theme style ---
-  toggle_style_key = '<leader>ts', -- Default keybinding to toggle
+  toggle_style_key = nil, -- Default keybinding to toggle
   toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' }, -- List of styles to toggle between
 
   -- Change code style ---
   -- Options are italic, bold, underline, none
   -- You can configure multiple style with comma seperated, For e.g., keywords = 'italic,bold'
   code_style = {
-    comments = 'none',
-    keywords = 'none',
-    functions = 'none',
+    comments = 'italic',
+    keywords = 'italic',
+    functions = 'bold',
     strings = 'none',
     variables = 'none'
   },
 
-  -- Custom Highlights --
-  colors = {}, -- Override default colors
-  highlights = {}, -- Override highlight groups
-
   -- Plugins Config --
   diagnostics = {
-    darker = false, -- darker colors for diagnostic
+    darker = true, -- darker colors for diagnostic
     undercurl = true, -- use undercurl instead of underline for diagnostics
     background = true, -- use background color for virtual text
   },
